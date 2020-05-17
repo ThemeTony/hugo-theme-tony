0889339873eb3d00d77fbb4f6d23ce1d68690527
FFRaycoder <nn_201312@163.com>
2020-05-17 13:33:36 +0800

    test travis

--- deploysrc.bat
+++ deploysrc.bat
@@ -1,15 +1,8 @@
 @echo off
-hugo --gc --minify --cleanDestinationDir
-cd public
-git add -A
 echo Commit: 
 set /p commit=
 ::echo %commit%
-git commit -m "%commit%"
-git push -u origin gh-pages -f
-cd ../themes/tony
 git add -A
-echo Commit: 
 git commit -m "%commit%"
-git push -u origin master -f
+git push -u origin site-sources -f
 close
