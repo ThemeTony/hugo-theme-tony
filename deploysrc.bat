9466b7b33e16f3eb6aa127e68cb396b13419d15e
FFRaycoder <nn_201312@163.com>
2020-05-17 13:57:09 +0800

    travis?

--- deploysrc.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Commit: 
-set /p commit=
-::echo %commit%
-git add -A
-git commit -m "%commit%"
-git push -u origin site-sources -f
-close
--- rebase.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd public
-git init
-git remote add origin git@github.com/ThemeTony/hugo-theme-tony.git
-git branch gh-pages
-git checkout gh-pages
-close
